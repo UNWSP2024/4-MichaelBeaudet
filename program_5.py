eaf4c8b7de72b82e740ce68f15201f5f9befad74
MichaelBeaudet <michaeltbeaudetalt@gmail.com>
2025-02-14 15:42:31 -0600

    Update program_5.py

--- program_5.py
+++ program_5.py
@@ -1,20 +1,29 @@
-# Program #5: Bank Balance
-# Write a program that asks the user to enter the amount that he or she has budgeted for a month.
-# A loop should then prompt the user to enter each of his or her expenses for the 
-# month and keep a running total. (Enter 0 to exit the loop)  
-# When the loop finishes, the program should display the amount that the 
-# user is over or under budget.
+# Author: Michael Beaudet
+# Title: Week 4 Program 5
+# Date 2/14/25
 
 def main():
     budget = 0.0
     difference = 0.0
-    spent = 1.0         #initialize for while loop
+    spent = 1.0         
     total = 0.0
 
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################
+    budget = float(input('Enter the budget for this month: '))
 
+    while True:
+        spent = float(input('Enter an amount you have spent (or type 0 to finish): '))
 
+        if spent == 0:
+            break
+        total += spent
+    difference = total - budget 
+    
+    if difference < 0:
+        print(f'You are under budget by: ${difference:.2f}')
+    elif difference > 0: 
+        print(f'You are over budget by: ${difference:.2f}')
+    else:
+        print('Congrats you are exactly on the budget!')
+    
 if __name__ == '__main__':
     main()
